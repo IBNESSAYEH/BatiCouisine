4b4dea4edb6a2d91a9442a2aefc766a00a30f77a
abdellatif ibnessayeh <abdellatifibnessayeh@gmail.com>
2024-09-19 12:13:14 +0100

    Implementation de MaterielRepositryImpl

--- src/com/BatiCouisine/repository/MainDoeuvreRepository.java
+++ src/com/BatiCouisine/repository/MainDoeuvreRepository.java
@@ -6,9 +6,9 @@ import java.util.Optional;
 
 public interface MainDoeuvreRepository {
 
-    void store(MainDoeuvreRepository mainDoeuvre, int idProjt);
+    void store(MainDoeuvre mainDoeuvre, int idProjt);
     Optional<MainDoeuvre> findById(int id);
-    void update(int id, MainDoeuvreRepository mainDoeuvre);
+    void update(int id, MainDoeuvre mainDoeuvre);
     void destroy(int id);
 
 }
