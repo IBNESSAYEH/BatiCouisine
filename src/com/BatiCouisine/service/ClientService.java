e73f77b76922ea50eb06fe604da66b5831ff4724
abdellatif ibnessayeh <abdellatifibnessayeh@gmail.com>
2024-09-19 15:12:23 +0100

    add ClientServiceImp/constructor

--- src/com/BatiCouisine/service/ClientService.java
+++ src/com/BatiCouisine/service/ClientService.java
@@ -1,4 +1,14 @@
 package com.BatiCouisine.service;
 
+import com.BatiCouisine.entities.Client;
+
+import java.util.List;
+import java.util.Optional;
+
 interface ClientService {
+    void store(Client client);
+    Optional<Client> findById(int id);
+    void update(int id, Client client);
+    void destroy(int id);
+    List<Client> findAll();
 }
--- src/com/BatiCouisine/service/Implementation/ClientServiceImp.java
+++ src/com/BatiCouisine/service/Implementation/ClientServiceImp.java
@@ -1,4 +1,13 @@
 package com.BatiCouisine.service.Implementation;
 
+import com.BatiCouisine.repository.ClientRepository;
+
 public class ClientServiceImp {
+    private ClientRepository clientRepository;
+
+    public ClientServiceImp(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+   
 }
