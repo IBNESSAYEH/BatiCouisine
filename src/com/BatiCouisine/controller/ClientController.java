8f384df53db6de6fcd8ef75c8ea38b72d91082f3
abdellatif ibnessayeh <abdellatifibnessayeh@gmail.com>
2024-09-20 09:49:38 +0100

    Ajout du ClientController avec les méthodes CRUD
    - Ajout du contrôleur  pour gérer les opérations CRUD sur les clients.
    - Méthode  pour ajouter un nouveau client via le service .
    - Méthode  pour récupérer un client par son ID.
    - Méthode  pour mettre à jour un client existant.
    - Méthode  pour supprimer un client.
    - Méthode  pour récupérer la liste de tous les clients.
    
    Le contrôleur utilise une injection de dépendance pour communiquer avec .

--- src/com/BatiCouisine/controller/ClientController.java
+++ src/com/BatiCouisine/controller/ClientController.java
@@ -1,4 +1,32 @@
 package com.BatiCouisine.controller;
 
+import com.BatiCouisine.entities.Client;
+import com.BatiCouisine.service.ClientService;
+
 public class ClientController {
+    private ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    public void store(Client client) {
+        clientService.store(client);
+    }
+
+    public void findById(int id) {
+        clientService.findById(id);
+    }
+
+    public void update(int id, Client client) {
+        clientService.update(id, client);
+    }
+
+    public void destroy(int id) {
+        clientService.destroy(id);
+    }
+
+    public void findAll() {
+        clientService.findAll();
+    }
 }
