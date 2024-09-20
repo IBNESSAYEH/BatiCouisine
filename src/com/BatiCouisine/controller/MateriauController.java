5d606402a458726d7fb403c113617372d1dd1746
abdellatif ibnessayeh <abdellatifibnessayeh@gmail.com>
2024-09-20 10:12:32 +0100

    Ajout du MateriauService avec les méthodes CRUD
    - Ajout du service  pour gérer les opérations CRUD sur les matériaux.
    - Méthode  pour ajouter un nouveau matériau.
    - Méthode  pour récupérer un matériau par son ID.
    - Méthode  pour mettre à jour un matériau existant.
    - Méthode  pour supprimer un matériau.
    - Méthode  pour récupérer la liste de tous les matériaux.
    
    Le service est conçu pour interagir avec la couche de données et fournir des fonctionnalités aux contrôleurs.

--- src/com/BatiCouisine/controller/MateriauController.java
+++ src/com/BatiCouisine/controller/MateriauController.java
@@ -1,4 +1,30 @@
 package com.BatiCouisine.controller;
 
+import com.BatiCouisine.entities.Materiau;
+import com.BatiCouisine.service.MateriauService;
+
 public class MateriauController {
+    private MateriauService materiauService;
+
+    public MateriauController(MateriauService materiauService) {
+        this.materiauService = materiauService;
+    }
+
+    public void store(Materiau materiau, int idProjt) {
+        materiauService.store(materiau, idProjt);
+    }
+
+    public void findById(int id) {
+        materiauService.findById(id);
+    }
+
+    public void update(int id, Materiau materiau) {
+        materiauService.update(id, materiau);
+    }
+
+    public void destroy(int id) {
+        materiauService.destroy(id);
+    }
+
+    
 }
